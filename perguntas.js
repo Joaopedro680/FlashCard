075e67507bcc767094acf6e580a3c3c286669ba0
Joaopedro680 <00001085284530sp@al.educacao.sp.gov.br>
2024-11-05 08:12:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual o filme mais assistido no cinmea no mundo?',
+    'Avatar'
+)
+
+criaCartao(
+    'Animais',
+    'Qual a raça do cavalo mais caro do Brasil?',
+    'Crioula'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o estopim para o início da Primeira Guerra Mundial?',
+    'O assassinato de Francisco Ferdinando da Áustria-Hungria'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a maior floresta do mundo?',
+    'Floreta Amazônica'
+)
